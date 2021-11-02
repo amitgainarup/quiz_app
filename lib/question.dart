100f97302e91a523575bc8853a5792ce5e066b72
Amit Gain <satabdimondal75@gmail.com>
2021-11-03 02:59:32 +0600

    Amit Gain

--- lib/question.dart
+++ lib/question.dart
@@ -2,17 +2,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Questions extends StatelessWidget {
-var questionText;
-Questions(this.questionText);
+  var questionText;
+  Questions(this.questionText);
   @override
   Widget build(BuildContext context) {
     return Text(
       questionText,
-    style: TextStyle(
-      color: Colors.blueAccent,
-      fontSize: 32,
-      fontFamily: 'IrishGrover',
-    ),
+      style: TextStyle(
+        color: Colors.blueAccent,
+        fontSize: 32,
+        fontFamily: 'IrishGrover',
+        shadows: [
+          Shadow(
+            color: Colors.deepOrange,
+            offset: Offset(5, 2),
+            blurRadius: 2,
+          ),
+        ],
+      ),
     );
   }
 }
